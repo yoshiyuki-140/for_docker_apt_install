1884ccfcacaa164d7e68e4e48bf35a0e525873b4
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-24 02:14:41 +0900

    install.shの追加

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 参考にしたサイト:https://kinsta.com/jp/blog/install-docker-ubuntu/
+
+# 既存パッケージのアップデート
+sudo apt update
+# 依存関係のインストール
+sudo apt install 
+# DockerのGPGの鍵をaptに登録する
+# これで、aptによる、インストールしたDockerパッケージの検証が可能になる
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# 実行すると、シェルの置換を利用して、AMD64やARM64など、システムのアーキテクチャが自動で検出され、
+# パッケージ情報がダウンロードされます。先ほど追加したGPG鍵を用いて認証が行われ、
+# リポジトリが、新たなパッケージリストとしてapt /etc/apt/sources.list.d directory.に追加されます。
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# aptがDockerパッケージの存在を検出できるよう、パッケージ情報を再度更新します。
+sudo apt update
+
+# やっとインストールすることができるようになったので、する。
+# docker-ce：Docker Engineのデーモン
+# docker-ce-cli：操作することになるDocker CLI
+# containerd.io：コンテナの起動と実行に利用するコンテナランタイム/ソフトウェア（containerd）
+sudo apt install docker-ce docker-ce-cli containerd.io
